ee0791ac4241095a80c4090d20e61b80e4a1f616
Yoichi Takai <contact@peacock0803sz.com>
2022-10-12 19:22:11 +0900

    :truck: Move handler for lambda

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,14 +1,23 @@
 from __future__ import annotations
 import logging
 import os
+print("loaded standard lib")
 
 from flask import Flask
+print("loaded flask")
+
+from pathlib import Path
+ls = list(Path(os.getcwd()).iterdir())
+print(f"{ls=}")
+
 
 from aws_sam_sandbox import routers  # noqa
 from aws_sam_sandbox.initilizer import init_app
+print("loaded myself")
 
 
 def main(is_debug: bool | None = None) -> Flask:
+    print("kicked funciton")
     if is_debug:
         logging.basicConfig(level=logging.DEBUG)
     setting = os.environ.get("SETTING", "local")
@@ -16,8 +25,8 @@ def main(is_debug: bool | None = None) -> Flask:
     return app
 
 
-def lambda_handler(event, context) -> Flask:
-    return main(is_debug=False)
+def lambda_handler(event, context) -> None:
+    main(is_debug=False)
 
 
 if __name__ == "__main__":
